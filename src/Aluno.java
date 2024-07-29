4ffcfecce0c89ff7536478b14202b73ad24d8f68
amanda-matoso <138587159+amanda-matoso@users.noreply.github.com>
2024-07-29 19:12:38 -0300

    Update Aluno.java

--- src/Aluno.java
+++ src/Aluno.java
@@ -1,5 +1,5 @@
-class Aluno {
-      private String curso;
+public class Aluno {
+    private String curso;
       private String matricula;
       private String descricao;
       private int id;
@@ -9,6 +9,20 @@ class Aluno {
       private String vinculo;
       private String telefone;
       private String projeto;
+
+    public Aluno (String curso, String matricula, String descricao, int id, String nome, String email, String senha, String vinculo, String telefone, String projeto){
+        setCurso(curso);
+        setDescricao(descricao);
+        setEmail(email);
+        setId(id);
+        setMatricula(matricula);
+        setNome(nome);
+        setProjeto(projeto);
+        setSenha(senha);
+        setTelefone(telefone);
+        setVinculo(vinculo);
+    }
+
 
       public void setCurso(String curso){
             if(curso != null){
@@ -106,24 +120,24 @@ class Aluno {
             }
         }
         
-        public String getProjeto(){
-            return this.projeto;
-        }
+    public String getProjeto(){
+        return this.projeto;
+    }
 
-      public void excluirAluno(String curso, String matricula, String descricao, int id, String nome, String email, String senha, String vinculo, String telefone, String projeto){
-            // Possibilita excluir aluno que não possuir mais vínculo com a instrituição do sistema.
-      }
+    public void excluirAluno(String curso, String matricula, String descricao, int id, String nome, String email, String senha, String vinculo, String telefone, String projeto){
+        // Possibilita excluir aluno que não possuir mais vínculo com a instrituição do sistema.
+    }
 
-      public void editarAluno(String curso, String matricula, String descricao, int id, String nome, String email, String senha, String vinculo, String telefone, String projeto){
-            // Possibilita editar informações erradas ou desatualizadas do cadastro do aluno.
-      }
+    public void editarAluno(String curso, String matricula, String descricao, int id, String nome, String email, String senha, String vinculo, String telefone, String projeto){
+        // Possibilita editar informações erradas ou desatualizadas do cadastro do aluno.
+    }
 
-//      public Reserva realizarReserva (Equipamento equipamento){
-//            // Possibilita ao aluno solicitar reservas de quipamentos.
-//      }
-//
-//      public Reserva realizarReserva (Espaco espaco) {
-//            // Possibilita ao aluno solicitar reservas de espaço.
-//      }
+    public Reserva realizarReserva (Equipamento equipamento){
+        // Possibilita ao aluno solicitar reservas de quipamentos.
+    }
+
+    public Reserva realizarReserva (Espaco espaco) {
+        // Possibilita ao aluno solicitar reservas de espaço.
+    }
 
 }
